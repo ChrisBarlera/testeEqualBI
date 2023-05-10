46f5ec50b2660577a3bad5ac94e98d596044dfdd
Christian Barlera <christian.barlera2@gmail.com>
2023-05-10 08:30:33 -0300

    Enviando arquivos

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # testeEqualBI
+Repositório contendo os arquivos utilizados para o processo seletivo da empresa EqualBI.
+
+## Sobre o desafio
+Trata-se do desenvolvimento de um *dashboard* a partir dos dados brutos de arquivos *.csv*. Era necessário também tratar os dados antes de usá-los.
+Os requisitos do *dashboard* estão melhor descritos no [PDF](CASE%20PS%2023.1.pdf).
+
+## Sobre o que foi desenvolvido
+Para poder importar estes arquivos no ambiente de trabalho escolhido (*Google Looker Studio*), era necessário primeiramente tratar os dados brutos para que fossem aceitos pelo sistema Google. <br>
+Para fazer isso, foi necessário escrever um [script em Python](limpaArquivoBI.py) para ler os arquivos, corrigir os problemas e então criar outros novos arquivos com os dados já tratados. <br>
+Com os novos arquivos, já foi possível desenvolver o [*dashboard*](https://lookerstudio.google.com/reporting/bdf2c213-3ee4-48ee-9177-a4c4e343a1d3) do desafio. 
+
+## Execução
+Neste tópico será abordado sobre a execução do script desenvolvido, detalhando quais são os pré-requisitos e os comandos a serem digitados no terminal.
+
+### Pré-requisitos
+- [Python](https://www.python.org/)
+- Conexão à internet
+
+### Comandos de terminal
+Já se encontrando no diretório que contém o arquivo `limpaArquivosBI.py`, basta digitar o seguinte comando para executá-lo.
+1. `python limpaArquivosBI.py`
--- limpaArquivoBI.py
+++ limpaArquivoBI.py
@@ -1,30 +1,38 @@
-nomeArquivo = input('Insira o nome do arquivo (Ex: teste.csv): ')
-nColunas = int(input('Insira o número de colunas da tabela: '))
-
-def postarLinha(novaLinha):
+# Checa se o arquivo existe ou não
+# Decide o modo para "abrir" o arquivo
+# Posta a linha
+def postarLinha(novaLinha, nomeArquivo):
     existe = False
     try: 
-        with open('new_'+nomeArquivo,'r') as arquivo:
+        with open('newFiles/new_'+nomeArquivo,'r') as arquivo:
             existe = True
     except:
         pass
 
     if existe:
-        with open('new_'+nomeArquivo,'a') as arquivo:
+        with open('newFiles/new_'+nomeArquivo,'a') as arquivo:
             arquivo.write(novaLinha)
     else:
-        with open('new_'+nomeArquivo,'w') as arquivo:
+        with open('newFiles/new_'+nomeArquivo,'w') as arquivo:
             arquivo.write(novaLinha)
 
+# Linhas que vinham com "" tinham problema pois eram linhas com , dentro de um campo e não exclusivamente para a divisão de colunas.
+def limpar(nomeArquivo, nColunas):
+    with open(nomeArquivo,'r') as arquivo:
+        for linha in arquivo.readlines():
+            cLinha = linha.split(',') # Separa uma linha por , atribuindo estes valores separados numa única lista
+            if len(cLinha) > nColunas: # Checa se o número de itens da lista é maior que o número de colunas (pois linhas podem ter vírgulas a mais usadas erroneamente)
+                # As linhas que tinham , extras tinham também "" usadas para delimitar a área da ,
+                # Separo a linha pelas aspas duplas, removo a , e posto a linha
+                inicioLinha = linha[:linha.index('\"')]
+                meioLinha = linha[linha.index('\"')+1:linha.index('\"',linha.index('\"')+1)].replace(',','.')
+                finalLinha = linha[linha.index('\"',linha.index('\"')+1)+1:]
+                cLinha = (inicioLinha+meioLinha+finalLinha).split(',')
+                postarLinha((inicioLinha+meioLinha+finalLinha),nomeArquivo)
+            else:
+                postarLinha(linha.replace('\"',''),nomeArquivo)
 
-with open(nomeArquivo,'r') as arquivo:
-    for linha in arquivo.readlines():
-        cLinha = linha.split(',')
-        if len(cLinha) > nColunas:
-            inicioLinha = linha[:linha.index('\"')]
-            meioLinha = linha[linha.index('\"')+1:linha.index('\"',linha.index('\"')+1)].replace(',','.')
-            finalLinha = linha[linha.index('\"',linha.index('\"')+1)+1:]
-            cLinha = (inicioLinha+meioLinha+finalLinha).split(',')
-            postarLinha((inicioLinha+meioLinha+finalLinha))
-        else:
-            postarLinha(linha.replace('\"',''))
+limpar('dim_familia_produtos.csv',2)
+limpar('dim_produtos.csv',3)
+limpar('dim_vendedores.csv',2)
+limpar('fato_vendas.csv',7)
